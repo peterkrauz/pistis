13fc589b69b077957738ec284db19140d8d841fd
Peter Krause <krausepeterm@gmail.com>
2022-05-31 18:03:43 -0300

    refactor: Delete redundant CommandRouter module

--- lib/pistis/server/request_handler.ex
+++ lib/pistis/server/request_handler.ex
@@ -1,5 +1,10 @@
 defmodule Pistis.Server.RequestHandler do
-  def process(message) do
-    Pistis.Server.CommandRouter.route_message(message)
+  def process(message), do: route_message(message)
+
+  defp route_message(message) do
+    case :ra.process_command(Pistis.Cluster.Manager.leader_node(), message) do
+      {:ok, response, _} -> response
+      error -> error
+    end
   end
 end
