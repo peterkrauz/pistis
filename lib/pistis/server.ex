5ffc0a13e5519496697ce927c1f4164a46ff6e92
Peter Krause <krausepeterm@gmail.com>
2022-06-10 21:24:56 -0300

    feat: Add @spec to Pistis.Server.send_request/1

--- lib/pistis/server.ex
+++ lib/pistis/server.ex
@@ -1,6 +1,9 @@
 defmodule Pistis.Server do
   alias Pistis.Request
+  alias Pistis.Response
   alias Pistis.Cluster.Manager, as: ClusterManager
+
+  @spec send_request(any) :: Response | tuple()
 
   def send_request(request_body) do
     case :ra.process_command(ClusterManager.leader_node(), %Request{body: request_body}) do
