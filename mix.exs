67d8031c816a6a96eb0709658fab8c8c564b2ae8
Peter Krause <krausepeterm@gmail.com>
2022-06-10 21:26:42 -0300

    release: Bump project version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pistis.MixProject do
   def project do
     [
       app: :pistis,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
