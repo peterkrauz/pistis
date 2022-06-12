6f2bec05f130fc3d9c58da3abcdc2cb5bc5536ba
Peter Krause <krausepeterm@gmail.com>
2022-06-11 21:04:04 -0300

    feat: Switch Raft leader node with random node on Pistis.Server.send_request/1

--- lib/pistis/server.ex
+++ lib/pistis/server.ex
@@ -6,7 +6,7 @@ defmodule Pistis.Server do
   @spec send_request(any) :: Response | tuple()
 
   def send_request(request_body) do
-    case :ra.process_command(ClusterManager.leader_node(), %Request{body: request_body}) do
+    case :ra.process_command(ClusterManager.any_node(), %Request{body: request_body}) do
       {:ok, response, _} -> response
       error -> error
     end
