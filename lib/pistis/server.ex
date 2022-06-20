f0c870c2a311ee9f9971ed78d671eb334cd168ea
Peter Krause <krausepeterm@gmail.com>
2022-06-20 09:53:47 -0300

    refactor: Simplify Cluster.Manager alias

--- lib/pistis/server.ex
+++ lib/pistis/server.ex
@@ -1,12 +1,12 @@
 defmodule Pistis.Server do
+  alias Pistis.Cluster.Manager
   alias Pistis.Machine.Request
   alias Pistis.Machine.Response
-  alias Pistis.Cluster.Manager, as: ClusterManager
 
   @spec send_request(any) :: Response | tuple()
 
   def send_request(request_body) do
-    case :ra.process_command(ClusterManager.any_node(), %Request{body: request_body}) do
+    case :ra.process_command(Manager.any_node(), %Request{body: request_body}) do
       {:ok, response, _} -> response
       error -> error
     end
